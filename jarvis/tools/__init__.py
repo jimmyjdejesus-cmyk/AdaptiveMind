12eb42b49ddaa8224dcee3da499e1f52f73b6257
rePoTaire <jimmy.j.dejesus@gmail.com>
2025-08-21 16:50:02 -0400

    Add tool modules and registry v2 with safety and docs

--- jarvis/tools/__init__.py
+++ jarvis/tools/__init__.py
@@ -7,6 +7,10 @@ including web research capabilities.
 from .repository_indexer import RepositoryIndexer
 from .environment_tools import read_file, write_file, run_shell_command, run_tests
 from .web_tools import WebSearchTool, WebReaderTool
+from .ide import open_file, save_file
+from .notes import add_note, list_notes
+from .github import create_issue
+from .fs_safety import ensure_path_is_allowed, safe_read, safe_write
 
 __all__ = [
     "RepositoryIndexer",
@@ -15,5 +19,13 @@ __all__ = [
     "run_shell_command",
     "run_tests",
     "WebSearchTool",
-    "WebReaderTool"
+    "WebReaderTool",
+    "open_file",
+    "save_file",
+    "add_note",
+    "list_notes",
+    "create_issue",
+    "ensure_path_is_allowed",
+    "safe_read",
+    "safe_write",
 ]
